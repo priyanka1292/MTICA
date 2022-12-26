0d20ab3ee4d140424258245ac7f8d4ed5e3257a1
priyanka1292 <121158505+priyanka1292@users.noreply.github.com>
2022-12-26 06:23:50 +0530

    Add files via upload

---
+++ day2program2.py
@@ -0,0 +1,10 @@
+pi=3.14159
+radius=int(input())
+height=int(input())
+base_area=pi*radius**2
+volume=round(base_area*height,4)
+surface_area=round(2*base_area+2*pi*radius*height,4)
+print("volume is",volume,"surface area is",surface_area)
+print(volume)
+print(surface_area)
+
