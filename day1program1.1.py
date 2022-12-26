751414e2bad6007e987a52fa8a99206a7de8d630
priyanka1292 <121158505+priyanka1292@users.noreply.github.com>
2022-12-26 09:39:02 +0530

    Add files via upload

---
+++ day1program1.1.py
@@ -0,0 +1,19 @@
+Python 3.11.0a5 (main, Feb  3 2022, 19:32:53) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+[10,20,'hello','c',8.9]
+[10, 20, 'hello', 'c', 8.9]
+help('return')
+
+help('non local')
+
+help('if')
+
+4+5
+9
+a=4
+b=5
+c=a+b
+c=a*b
+c=a-b
+print(c)
+-1
