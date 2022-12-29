9a6dc31082549f43cdf34cded443b4f58bcbdc68
priyanka1292 <121158505+priyanka1292@users.noreply.github.com>
2022-12-29 09:33:51 +0530

    Add files via upload

---
+++ day7program21.py
@@ -0,0 +1,9 @@
+##def cube(a):
+##    return a**3
+##lst=[0,1,2,3,4,5,6,7]
+##res=list(map(cube,lst))
+##print(res)
+
+lst=[0,1,2,3,4,5,6,7]
+res1=list(map(lambda x:x**3,lst))
+print(res1)
---
+++ day7program22.py
@@ -0,0 +1,3 @@
+lst=['apple','gua','jamakaya','mango','papaya','orange']
+res_lst=list(map(len,lst))
+print(res_lst)
